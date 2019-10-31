948862be6dbbda5b396deb277f6d6cc89397edc8
Timomam <55140402+Timomam@users.noreply.github.com>
2019-10-31 12:08:04 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+    
+    <head>
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <meta charset="UTF-8">
+        <meta lang="en"> 
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
+        <title>Mitä opin tänään</title>
+    </head>
+
+    <body>
+        <header>
+            <h1>Mitä opin tänään</h1>
+        </header>
+        <div id="container">
+        <form>
+            <input id="field" type="text">
+            <input id="button" type="button" value="Lisää uusi">
+          </form>
+          <div id="table">
+          <table style="width:80%">
+            <tr>
+              <th class="left">Päivä</th>
+              <th class="center">Oivallus</th> 
+              <th class="right">Poista</th>
+            </tr>
+            <tr>
+                <td>31.10.2019</td>
+                <td>https://github.com/Timomam/Mita-opin-tanaan</td>
+                <td><input type="button" value="X"></td>
+            </tr>
+          </table>
+        </div>
+      </div>
+          <footer>
+            Copyright 2019 Lorem Ipsum
+          </footer>
+    </body>
+
+</html>
